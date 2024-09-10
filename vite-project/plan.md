8793a894ea39d22c5a46818d927b7407f0636c48
Danielle <168420921+danielleaop@users.noreply.github.com>
2024-09-10 12:32:16 +0100

    plan and trying to link project with supabase account using env file

---
+++ vite-project/plan.md
@@ -0,0 +1,6 @@
+1. start a project with vite
+2. run npm install and check server is running
+3. install supabase 
+4. start a project in supabase 
+5. link this to our project and use the .env file to do this
+6. import supabase module and initialise supabase database and auth
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,10 @@
 import "./App.css";
+import 'dotenv/config';
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient('https://<vite-project>.supabase.co', '<your-anon-key>')
 
 function App() {
   return <></>;
