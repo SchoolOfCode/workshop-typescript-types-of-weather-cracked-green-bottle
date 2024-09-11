8ff090e4b5d259da8d65192dbac916793c6e6b99
Marco Di Summa <mdisumma@yahoo.com>
2024-09-11 12:28:27 +0100

    implement onChange, OnClick

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,36 +1,36 @@
-import './App.css'
-import React, { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
 
-
-  const [inputValue, setInputValue]=useState('')
-
-  function handleclick(event){
+  function handleclick(event: any) {
     event.preventDefault();
-    console.log(event.target.value)
+    console.log("initializate our API");
   }
 
-
-  function handleChange(event){
-    setInputValue(event.target.value)
-    console.log(inputValue)
+  function handleChange(event: any) {
+    setInputValue(event.target.value);
+    console.log(inputValue);
   }
-
 
   return (
     <main>
-
       <h1>Weather App</h1>
       <form>
-        <label>Insert your city
-          <input type="text" name="country" onChange={(event)=>handleChange(event)} value={inputValue}></input>
+        <label>
+          Insert your city
+          <input
+            type="text"
+            name="country"
+            onChange={(event) => handleChange(event)}
+            value={inputValue}
+          ></input>
         </label>
-        <button onClick={(event)=>handleclick(event)}>Search</button>
+        <button onClick={(event) => handleclick(event)}>Search</button>
       </form>
-       
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
