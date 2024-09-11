b0bbc27813b486267fec5c5cc482cdd198343991
Marco Di Summa <mdisumma@yahoo.com>
2024-09-11 15:31:59 +0100

    implementig the weather api

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,66 +3,50 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [apiValues, setAPIValues] = useState("");
-  const [coords, setCoords] = useState("")
+  const [coords, setCoords] = useState("");
+  const [weather, setWeather] = useState(false);
 
-// useEffect (() => {
-//   fetch('http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&appid=48404bfdde79b99ab720c89112005316')
-//    .then(response => response.json())
-//    .then(data => {setAPIValues(data); console.log(apiValues)})
-
-// }, [inputValue])
-   
-   
-
-  
-  async function handleCord (){
-
-    const url= `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&appid=48404bfdde79b99ab720c89112005316`
-
-    try {
-    const response= await fetch(url);
-    const jsonData= await response.json();
-    setAPIValues(jsonData);
-    setCoords({lat:jsonData[0].lat, lon:jsonData[0].lon});
-    console.log(coords)
-    }
-
-    catch (error){
-      console.error(error.message)
-    }
-
+  //get input
+  function handleChange(event: any) {
+    setInputValue(event.target.value);
+    console.log(inputValue);
   }
 
-  async function getWeather(){
-    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=48404bfdde79b99ab720c89112005316`
-
+  //get coordinate
+  async function handleCord() {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&appid=48404bfdde79b99ab720c89112005316`;
     try {
-      const response= await fetch(url)
-      const jsonData= await response.json();
-      console.log(jsonData)
-    }
-
-    catch (error) {
-      console.error(error.message)
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      setCoords({ lat: jsonData[0].lat, lon: jsonData[0].lon });
+      console.log(coords);
+    } catch (error) {
+      console.error(error.message);
     }
   }
 
+  //get weather
+  async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=48404bfdde79b99ab720c89112005316`;
+    try {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setWeather(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  // useEffect(() => {
+  //   inputValue && getWeather();
+  // }, [inputValue, coords, weather]);
+
+  // click event
   async function handleclick(event: any) {
     event.preventDefault();
     await handleCord();
     await getWeather();
-
-  }
-
-
-    
-
-
-
-  function handleChange(event: any) {
-    setInputValue(event.target.value);
-    console.log(inputValue);
   }
 
   return (
@@ -80,6 +64,8 @@ function App() {
         </label>
         <button onClick={(event) => handleclick(event)}>Search</button>
       </form>
+      {weather ? <p>true</p> : <p>false</p>}
+      {/* <p>{weather.weather[0].main}</p> */}
     </main>
   );
 }
